6192d6f75c46b4619f34d155ddcaeea0d4a294b1
hailie <20181011@sungshin.ac.kr>
2021-09-17 22:22:46 +0900

    feat: 이력 등록/조회 ing

--- server/api-routes/vaccincation.js
+++ server/api-routes/vaccincation.js
@@ -2,30 +2,22 @@ const express = require('express');
 const router = express.Router();
 var sdk = require('../sdk');
 const { user } = require('../models');
-//var ㅁㅁservice = require('../service/ㅁㅁ');
 
 
 router.post('/', async (req, res, next) => {
-    let record = req.body;
-    res.status(201).send({
-        message: '접종 이력이 등록되었습니다.',
-    });
-});
+    let { date, location, vaccine_type, vaccine_session } = req.body;
+    let user_id = "test@naver.com"
+    
+    // 중복 체크 어떻게 할 것 인지?
 
-// http://54.180.199.56:8080/api/v1/vaccincation/?user_id=h@naver.com&date=2020-01-01&vaccine_type=test&vaccine_session=1
-router.get('/', async (req, res, next) => {
-    var record = req.query;
-
-    // 값 검사 필요, 중복 처리 필요
-
-    // 이메일, 날짜, 백신종류, 백신차수
     let args = [
-        record.user_id,
-        record.date,
-        record.vaccine_type,
-        record.vaccine_session,
+        user_id,
+        date,
+        vaccine_type,
+        vaccine_session,
     ];
-    var result = await sdk.send(true, 'putCertificate', args); // true: res.send('success');
+
+    let result = await sdk.send(true, 'putCertificate', args); // true: res.send('success');
 
     if (result == 'success') {
         res.status(201).send({});
@@ -35,6 +27,15 @@ router.get('/', async (req, res, next) => {
 });
 
 router.get('/vaccine/:vaccineIndex', async (req, res, next) => {
+    let {vaccineIndex} = req.params
+
+    let args = [vaccineIndex];
+    let result = await sdk.send(false, 'getCertificateByCertKey', args); 
+    let resultJSON = JSON.parse(result);
+    res.json({
+        records: resultJSON,
+    });
+    /*
     res.json({
         date: '2021-08-16',
         location: '서울아산병원',
@@ -43,48 +44,18 @@ router.get('/vaccine/:vaccineIndex', async (req, res, next) => {
         user_id: 'bpm1234@gmail.com',
         vaccine_index: 'vc01',
     });
+    */
 });
 
 router.get('/:userId', async (req, res, next) => {
-    var { userId } = req.params; //var vaccineIndex = req.params.vaccineIndex;
+    let user_id = "test@naver.com"
 
-    let args = [userId];
-    var result = await sdk.send(false, 'getCertificateByUserId', args); // false: res.send(result.toString());
-    var resultJSON = JSON.parse(result.toString('utf8'));
-    var rJ = JSON.parse(result);
+    let args = [user_id];
+    let result = await sdk.send(false, 'getCertificateByUserId', args); 
+    let resultJSON = JSON.parse(result);
     res.json({
-        records: rJ,
+        records: resultJSON,
     });
-
-    /*
-    res.json({
-        records: [
-            {
-                date: '2021-08-16',
-                location: '서울아산병원',
-                vaccine_type: '화이자',
-                vaccine_session: 1,
-                user_id: 'bpm1234@gmail.com',
-                vaccine_index: 'vc01',
-            },
-            {
-                date: '2021-08-16',
-                location: '서울아산병원',
-                vaccine_type: '화이자',
-                vaccine_session: 2,
-                user_id: 'bpm1234@gmail.com',
-                vaccine_index: 'vc02',
-            },
-            {
-                date: '2021-08-16',
-                location: '서울아산병원',
-                vaccine_type: '화이자',
-                vaccine_session: 1,
-                user_id: 'bpm1234@gmail.com',
-                vaccine_index: 'vc01',
-            },
-        ],
-    });*/
 });
 
 module.exports = router;
