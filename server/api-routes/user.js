814540a116c14802c099b520aeb41a01e5a66492
hailie <20181011@sungshin.ac.kr>
2021-09-17 22:17:09 +0900

    feat: /join 개발 ing
    - 완성: 이메일 중복 처리 & db 삽입 & 토큰 전송
    - 미완: 랜덤 인증코드 생성 & 이메일 전송

--- server/api-routes/user.js
+++ server/api-routes/user.js
@@ -4,7 +4,7 @@ const { jwtKey } = require('../config/config');
 const router = express.Router();
 const authMiddleware = require('../middlewares/auth');
 const { user } = require('../models');
-var userService = require('../services/user');
+const userService = require('../services/user');
 
 router.get('/dbtest', async (req, res, next) => {
     try {
@@ -14,35 +14,67 @@ router.get('/dbtest', async (req, res, next) => {
         res.status(400).send({ message: 'DB not Connection' });
     }
 });
+
 router.post('/join', async (req, res, next) => {
-    var { user_id, phone, name, birth, gender } = req.body;
+    let { user_id, phone, name, birth, gender } = req.body;
 
-    // db 처리, 인증코드 생성 이메일 전송
+    const existUsers = await user.findAll({
+        where: {
+            email: user_id,
+        },
+    });
+    if (existUsers.length) {
+        res.status(400).send({
+            message: '이미 가입된 이메일 입니다.',
+        });
+        return;
+    }
 
-    const token = jwt.sign({ userIdx: 1 }, jwtKey);
-    res.send({
+    // 인증코드 생성
+    let code = 'TEst12';
+
+    const newUser = await user.create({
+        email: user_id,
+        phone: phone,
+        name: name,
+        birth: birth,
+        gender: gender,
+        verification_number: code,
+        created_at: new Date,
+        updated_at: new Date
+    });
+
+    // 이메일 전송
+
+    const token = jwt.sign({ userIdx: newUser.idx_user }, jwtKey);
+    res.status(201).send({
         token,
     });
 });
 
 router.patch('/auth', authMiddleware, async (req, res, next) => {
-    var { code } = req.body;
+    let { code } = req.body;
     const { idx_user, verification_number } = res.locals.user;
 
     try {
         if (verification_number == code) {
-            await user.update({ sign_up_verification: 1 },{
-                where: {
-                    idx_user: idx_user
+            await user.update(
+                { sign_up_verification: 1 },
+                {
+                    where: {
+                        idx_user: idx_user,
+                    },
                 }
-            });
-    
+            );
+
             res.status(201).send({});
         } else {
             res.status(400).send({ message: '인증코드가 일치하지 않습니다.' });
         }
     } catch (error) {
-        res.status(400).send({ message: '알 수 없는 오류가 발생하였습니다. 관리자에게 문의하세요.' });
+        res.status(400).send({
+            message: '알 수 없는 오류가 발생하였습니다. 관리자에게 문의하세요.',
+        });
     }
 });
 
