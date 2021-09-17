a866bf75e601850dc236647d47f0491722e38cee
hailie <20181011@sungshin.ac.kr>
2021-09-17 20:08:00 +0900

    chore: db 수정
    User table 수정

--- server/models/user.js
+++ server/models/user.js
@@ -43,6 +43,10 @@ module.exports = function(sequelize, DataTypes) {
       type: DataTypes.STRING(45),
       allowNull: true
     },
+    verification_number: {
+      type: DataTypes.STRING(100),
+      allowNull: false
+    },
     sign_up_verification: {
       type: DataTypes.TINYINT,
       allowNull: false,
