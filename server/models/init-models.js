079a3cb067ed67b7180e6875f68b5786e91c3d0d
hailie <20181011@sungshin.ac.kr>
2021-09-20 00:02:54 +0900

    chore: db 수정(group table 추가), blockchain user 추가

--- server/models/init-models.js
+++ server/models/init-models.js
@@ -1,18 +1,17 @@
 var DataTypes = require("sequelize").DataTypes;
 var _follow = require("./follow");
+var _group = require("./group");
 var _user = require("./user");
 
 function initModels(sequelize) {
   var follow = _follow(sequelize, DataTypes);
+  var group = _group(sequelize, DataTypes);
   var user = _user(sequelize, DataTypes);
 
-  follow.belongsTo(user, { as: "following", foreignKey: "following_id"});
-  user.hasMany(follow, { as: "follows", foreignKey: "following_id"});
-  follow.belongsTo(user, { as: "followed", foreignKey: "followed_id"});
-  user.hasMany(follow, { as: "followed_follows", foreignKey: "followed_id"});
 
   return {
     follow,
+    group,
     user,
   };
 }
