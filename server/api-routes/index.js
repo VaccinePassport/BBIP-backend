565c84a5439045947a73a7cf41f1186cf9ad0f3f
hailie <20181011@sungshin.ac.kr>
2021-10-05 00:15:07 +0900

    [fix] device token router 에러 수정

--- server/api-routes/index.js
+++ server/api-routes/index.js
@@ -1,9 +1,11 @@
 const userRouter = require('./user');
 const vaccincationRouter = require('./vaccincation');
 const qrRouter = require('./qr');
+const deviceTokenRouter = require('./deviceToken');
 
 module.exports = {
     userRouter,
     vaccincationRouter,
-    qrRouter
+    qrRouter,
+    deviceTokenRouter,
 };
