49165ec923dd234716a9ea5ad8f408dc918391e7
jjangdol06 <jiyeon971023@gmail.com>
2021-10-04 17:16:06 +0900

    fix :  create deviceTokenService

--- server/api-routes/deviceToken.js
+++ server/api-routes/deviceToken.js
@@ -2,8 +2,8 @@ const express = require('express');
 const { authMiddleware } = require('../middlewares');
 const deviceTokenService = require('../services/deviceToken');
 
-const router = express.Router(deviceTokenService.save);
+const router = express.Router();
 
-router.post('/device', );
+router.post('/device', deviceTokenService.save);
 
 module.exports = router;
