c5f4c8dbbbe7bbfb4db05ee290757ea3d385cfd9
jjangdol06 <jiyeon971023@gmail.com>
2021-10-29 12:21:54 +0900

    fix : for문으로 list 반환

--- server/services/qr/verifyQr.js
+++ server/services/qr/verifyQr.js
@@ -13,12 +13,14 @@ const verifyQr = {
             console.log(vaccine_index.vaccine_index[0].vaccine_index)
             const vaccine_info = await verifyQr.getVaccineByIndex(vaccine_index.vaccine_index[0].vaccine_index)
 
+            // 푸쉬알림 : 검증 결과 성공
             res.json({
                 verified_data: vaccine_info
             });
 
         } catch (error) {
             console.log(error);
+            // 푸쉬알림 : 검증 결과 실패
             res.status(400).json({
                 message: '알 수 없는 오류가 발생했습니다.',
             });
@@ -32,16 +34,19 @@ const verifyQr = {
         let resultJSON = JSON.parse(result);
         console.log("json : " + resultJSON[0].vaccineKey)
 
-        return {
-            date: resultJSON[0].record.date,
-            location: resultJSON[0].record.location,
-            vaccine_type: resultJSON[0].record.vaccinetype,
-            vaccine_session: parseInt(
-                resultJSON[0].record.vaccinenumber
-            ),
-            user_id: resultJSON[0].record.userid,
-            vaccine_index: resultJSON[0].vaccineKey,
+        let vaccineList = [];
+        for (let vaccine of resultJSON) {
+            vaccineList.push({
+                date: vaccine.record.date,
+                location: vaccine.record.location,
+                vaccine_type: vaccine.record.vaccinetype,
+                vaccine_session: parseInt(vaccine.record.vaccinenumber),
+                user_id: vaccine.record.userid,
+                vaccine_index: vaccine.vaccineKey,
+            });
         }
+
+        return vaccineList
    },
      
 };
