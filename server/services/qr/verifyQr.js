7f2a064f89daa9d9d8bfe06820ffe69d4a529a74
jjangdol06 <jiyeon971023@gmail.com>
2021-10-29 09:35:15 +0900

    bug-fix: 백신 인덱스 값 변수명 변경

--- server/services/qr/verifyQr.js
+++ server/services/qr/verifyQr.js
@@ -10,9 +10,7 @@ const verifyQr = {
             let { qrVaccine } = req.params;
             
             const vaccine_index = verifyJWT.verifyQrContent(qrVaccine);
-            console.log(vaccine_index)
-            const vaccine_info = await verifyQr.getVaccineByIndex(vaccine_index);
-            console.log(vaccine_info)
+            const vaccine_info = await verifyQr.getVaccineByIndex(vaccine_index.vaccine_index)
 
             res.json({
                 verified_data: vaccine_info
