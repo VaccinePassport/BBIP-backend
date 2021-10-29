c2da72f9da2c05894f36bae8b1d306d2dc28a44b
jjangdol06 <jiyeon971023@gmail.com>
2021-10-29 12:07:49 +0900

    test: 로그 확인

--- server/services/qr/verifyQr.js
+++ server/services/qr/verifyQr.js
@@ -30,7 +30,7 @@ const verifyQr = {
         let args = [String(index)];
         let result = await sdk.send(true, 'getCertificateByCertKey', args);
         let resultJSON = JSON.parse(result);
-        console.log(resultJSON)
+        console.log(resultJSON[0].vaccineKey)
 
         return {
             date: resultJSON[0].record.date,
