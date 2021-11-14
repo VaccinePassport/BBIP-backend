33ed48c8c8f87dabf00dc60690ac33fff7bb18d9
hailie <20181011@sungshin.ac.kr>
2021-11-14 14:12:55 +0900

    [fix] api-route 순서 변경

--- server/api-routes/friends.js
+++ server/api-routes/friends.js
@@ -5,12 +5,14 @@ const friendsService = require('../services/friends');
 const router = express.Router();
 
 router.get('/list', friendsService.getFriendList);
-router.get('/:userId', friendsService.searchFriend);
+router.get('/request', friendsService.getFollowRequests);
+
 router.post('/', friendsService.registerFriends);
 router.patch('/', friendsService.bookmarkFriends);
 router.delete('/', friendsService.deleteFriends);
 router.get('/vaccination/:friendId', friendsService.getVaccinationInfo);
 
-router.get('/request', friendsService.getFollowRequests);
+
 router.patch('/accept',friendsService.acceptFollowRequest);
+router.get('/:userId', friendsService.searchFriend);
 module.exports = router;
