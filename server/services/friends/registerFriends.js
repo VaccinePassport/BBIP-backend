160b5d8fc69687f8a44e5cb92009bf17232d2877
jjangdol06 <jiyeon971023@gmail.com>
2021-11-16 23:08:37 +0900

    fix : 탈퇴한 회원 accept 0으로 되돌리기

--- server/services/friends/registerFriends.js
+++ server/services/friends/registerFriends.js
@@ -18,7 +18,6 @@ module.exports = async (req, res, next) => {
 
         await registerFriends(user.idx_user, friend[0].get('idx_user'));
         deviceToken = await findFriendDeviceToken(friend_id)
-        console.log(deviceToken[0])
         if (deviceToken[0]) {
             push.pushAlarm([deviceToken[0].get('device_token')], `[BBIP]동행인 등록 요청`, `${friend_id}님이 동행인 등록을 요청하셨습니다. 동의하시나요?`);
         } else {
@@ -46,8 +45,21 @@ const registerFriends = async (followingIdx, followedIdx) => {
 
         if (exFollow[0]) {
             console.log('이미 존재하는 동행인');
+            //-1 -> 0으로 update하기
+            if (exFollow[0].get('accept') == -1) {
+                await Follow.update(
+                    {
+                        accept: 0
+                    },
+                    {
+                        where: {
+                            following_id: followingIdx,
+                            followed_id: followedIdx
+                        }
+                    }
+                );
+            }
         } else {
-
             const exFollow2 = await Follow.findAll({
                 where: {
                     following_id: followedIdx,
@@ -57,18 +69,35 @@ const registerFriends = async (followingIdx, followedIdx) => {
 
             if (exFollow2[0]) {
                 console.log('이미 존재하는 동행인');
-                await Follow.update(
-                    {
-                        following_id: followingIdx,
-                        followed_id: followedIdx
-                    },
-                    {
-                        where: {
-                            following_id: followedIdx,
-                            followed_id: followingIdx
+                // -1 -> 0으로 update
+                if (exFollow[0].get('accept') == -1) {
+                    await Follow.update(
+                        {
+                            accept: 0,
+                            following_id: followingIdx,
+                            followed_id: followedIdx
+                        },
+                        {
+                            where: {
+                                following_id: followingIdx,
+                                followed_id: followedIdx
+                            }
                         }
-                    }
-                );
+                    );
+                } else {
+                    await Follow.update(
+                        {
+                            following_id: followingIdx,
+                            followed_id: followedIdx
+                        },
+                        {
+                            where: {
+                                following_id: followedIdx,
+                                followed_id: followingIdx
+                            }
+                        }
+                    );
+                }
 
             } else {
                 await Follow.create({
